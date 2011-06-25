68b62218258fa770401355519e1514db8fa408e6
Jacob Rask <jacob@jacobrask.net>
2011-06-25 14:25:29 +0200

    Removing a number of unused listings.
    None of my devices show MediaTomb's descriptions either, so removing
    that as well.

--- import.js
+++ import.js
@@ -2,95 +2,33 @@
 
 function addAudio(obj) {
  
-    var title, artist, album, date, genre, description, track, chain,
-        artist_full, album_full, temp,
-        desc = '';
+    var title, artist, album, date, genre, chain;
 
 
-    // first gather data
-
+    // Define metadata variables
     title = obj.meta['M_TITLE'] || obj.title;
+    artist = obj.meta['M_ARTIST'] || '[unknown]';
+    album = obj.meta['M_ALBUM'] || '[unknown]';
+    date = getYear(obj.meta['M_DATE']) || '0000';
+    genre = obj.meta['M_GENRE'] || '[unknown]';
 
-    artist = obj.meta['M_ARTIST'];
-    if (!artist) {
-        artist = 'Unknown';
-        artist_full = null;
-    } else {
-        artist_full = artist;
-        desc = artist;
-    }
-    
-    album = obj.meta['M_ALBUM'];
-    if (!album) {
-        album = 'Unknown';
-        album_full = null;
-    } else {
-        desc = desc + ', ' + album;
-        album_full = album;
-    }
-    
-    if (desc) {
-        desc = desc + ', ';
-    }
-    
-    desc = desc + title;
-    
-    date = obj.meta['M_DATE'];
-    if (!date) {
-        date = 'Unknown';
-    } else {
-        date = getYear(date);
-        desc = desc + ', ' + date;
-    }
-    
-    genre = obj.meta['M_GENRE'];
-    if (!genre) {
-        genre = 'Unknown';
-    } else {
-        desc = desc + ', ' + genre;
-    }
-    
-    description = obj.meta['M_DESCRIPTION'] || desc;
-
-    track = '';
-
-    chain = ['Audio', 'All Audio'];
-    obj.title = title;
+    // Add "all songs" to artist listings
+    chain = ['Audio', 'Artists', artist, '(all songs)'];
     addCdsObject(obj, createContainerChain(chain));
     
-    chain = ['Audio', 'Artists', artist, 'All Songs'];
-    addCdsObject(obj, createContainerChain(chain));
-    
-    chain = ['Audio', 'All - full name'];
-    temp = '';
-    if (artist_full) {
-        temp = artist_full;
-    }
-    if (album_full) {
-        temp = temp + ' - ' + album_full + ' - ';
-    } else {
-        temp = temp + ' - ';
-    }
-
-    obj.title = temp + title;
-    addCdsObject(obj, createContainerChain(chain));
-    
-    chain = ['Audio', 'Artists', artist, 'All - full name'];
-    addCdsObject(obj, createContainerChain(chain));
-    
+    // Add albums to artists listing
     chain = ['Audio', 'Artists', artist, album];
-    obj.title = track + title;
+    obj.title = title;
     addCdsObject(obj, createContainerChain(chain), 'UPNP_CLASS_CONTAINER_MUSIC_ALBUM');
     
+    // Add root Albums listing
     chain = ['Audio', 'Albums', album];
     obj.title = track + title; 
     addCdsObject(obj, createContainerChain(chain), 'UPNP_CLASS_CONTAINER_MUSIC_ALBUM');
     
-    chain = ['Audio', 'Genres', genre];
+    // Add root Genre" listing
+    chain = ['Audio', 'Genre - songs', genre];
     addCdsObject(obj, createContainerChain(chain), 'UPNP_CLASS_CONTAINER_MUSIC_GENRE');
-    
-    chain = ['Audio', 'Year', date];
-    addCdsObject(obj, createContainerChain(chain));
 }
 
 function addVideo(obj) {
